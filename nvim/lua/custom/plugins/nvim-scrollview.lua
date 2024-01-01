172a74512c07fbf2ef1d76b511a2a18672a8fc4e
mushfiq <mushfiqxrabbi@gmail.com>
2024-01-02 00:44:40 +0600

    fix debounce

--- nvim/lua/custom/plugins/nvim-scrollview.lua
+++ nvim/lua/custom/plugins/nvim-scrollview.lua
@@ -1,8 +1,9 @@
 return {
-  'dstein64/nvim-scrollview',
+  "dstein64/nvim-scrollview",
   config = function()
-    require('scrollview').setup {}
-    vim.g.scrollview_always_show = true
-    vim.g.scrollview_excluded_filetypes = { 'alpha', 'minimap' }
+    require("scrollview").setup({
+      scrollview_always_show = true,
+      scrollview_excluded_filetypes = { "alpha" },
+    })
   end,
 }
